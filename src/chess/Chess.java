01adebe916a8ea8e44a293e13c5d8712c27b270e
teddy <teddy@localhost>
2025-04-14 14:54:46 +1200

    Testing

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -12,8 +12,9 @@ public class Chess {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        System.out.println("hello");
     }
     
 }
